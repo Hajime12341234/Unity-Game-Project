52a0a0f58afa67ba337e6987b5c62d30332bd54f
Hajime12341234 <132004336+Hajime12341234@users.noreply.github.com>
2024-07-14 00:14:13 -0400

    my first commit

---
+++ Assets/Scripts/Player_controller.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/*
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+*/
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10.0f;
+
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        transform.Translate(movement * speed * Time.deltaTime, Space.World);
+    }
+}
+
